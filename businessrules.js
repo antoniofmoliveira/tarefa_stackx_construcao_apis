faa070f1b109e7f43970dc156c3d463f91d4b2be
antoniofmoliveira <antoniofmoliveia@outlook.com>
2024-10-02 14:42:56 -0300

    feat: tarefa 1 - add persistencia

--- businessrules.js
+++ businessrules.js
@@ -1,18 +1,48 @@
+import fs from 'fs'
+
 let data = []
 let lastId = 0
+
+
+function saveDataToFile() {
+    fs.writeFileSync('data.json', JSON.stringify(data), (err) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+    })
+}
+
+function loadDataFromFile() {
+    try {
+        data = JSON.parse(fs.readFileSync('data.json', 'utf8'))
+        for (let i = 0; i < data.length; i++) {
+            if (data[i].id > lastId) {
+                lastId = data[i].id
+            }
+        }
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+
 
 function createItem(item) {
     lastId += 1
     item.id = lastId
     data.push(item)
+    saveDataToFile()
     return item
 }
 
 function readItems() {
+    loadDataFromFile()
     return data
 }
 
 function readItemById(id) {
+    loadDataFromFile()
     return data.find(item => item.id === parseInt(id))
 }
 
@@ -20,6 +50,7 @@ function updateItem(id, updatedItem) {
     const index = data.findIndex(item => item.id === parseInt(id))
     if (index !== -1) {
         data[index] = { ...data[index], ...updatedItem, id: data[index].id }
+        saveDataToFile()
         return data[index]
     }
     return null
@@ -29,6 +60,7 @@ function deleteItem(id) {
     const index = data.findIndex(item => item.id === parseInt(id))
     if (index !== -1) {
         const deletedItem = data.splice(index, 1)
+        saveDataToFile()
         return deletedItem[0]
     }
     return null
