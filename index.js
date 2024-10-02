f5610124932e544657300c419a05af077d0f30aa
antoniofmoliveira <antoniofmoliveia@outlook.com>
2024-10-02 13:52:41 -0300

    preparado para crud

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
 import Express from 'express';
 
 const server = Express();
+const port = 1234;
 
 server.use(Express.json());
 
-server.get("/:nome", (req, res) => {
-    res.status(200).send(req.params.nome);
-});
 
-server.post("/", (req, res) => {
-    const { nome, idade, cidade } = req.body;
 
-    if (!nome || !idade || !cidade) {
-        res.status(400).send("Está faltando dados");
-    }
-
-    const valorRetorno = `Seu nome é ${nome}, você tem ${idade} anos e mora na cidade ${cidade}`;
-
-    res.status(200).send(valorRetorno);
-})
-
-server.listen(1234, () => console.log("Servidor escutando na porta 1234"));
+server.listen(port, () => console.log(`Servidor escutando na porta ${port}`));
 
